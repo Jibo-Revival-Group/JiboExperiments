a4081cf3e6f40e7404c0dbdc6d72d13d64165ecd
Jake Dubin <jacob.dubin@transcendentsoftware.com>
2026-07-17 00:12:57 -0500

    Update Deploy-OpenJiboManaged.ps1

--- OpenJibo/scripts/cloud/Deploy-OpenJiboManaged.ps1
+++ OpenJibo/scripts/cloud/Deploy-OpenJiboManaged.ps1
@@ -223,7 +223,7 @@ function Bind-ContainerAppHostname {
         --name $ContainerAppName `
         --hostname $Hostname `
         --environment $ManagedEnvironmentName `
-        --validation-method CNAME `
+        --validation-method TXT `
         --output none
     Write-Host "Managed certificate binding completed for hostname '$Hostname'."
 }
